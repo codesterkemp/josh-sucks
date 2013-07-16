263ff4e59eb60c466036ea165af5e4af5c89d49a
codesterkemp <sparky_csk@yahoo.com>
2013-07-16 19:31:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewFire::Application.config.secret_token = '6e8f197277c95727b10a9c0c76e2f596d2ea430c6d74788454be8bde0a8a74358b1345fd981162783fb78ff83017a7843f0af4a148f2d35021a787dcc6bdb312'
